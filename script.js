f244bfe26c07e8ed00b133bfd8ef4f9e4dd4511c
ArianQazvini <a.qazvini@aut.ac.ir>
2024-01-12 22:44:59 +0330

    adding comments

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// Function to be executed when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded',function () {
+    // Retrieving elements from the DOM
     var nameInput = document.getElementById('name');
     var submitButt = document.getElementById('submit-but');
     var saveButt = document.getElementById('save-but');
@@ -12,13 +14,16 @@ document.addEventListener('DOMContentLoaded',function () {
     var clearSavedButt = document.getElementById('clear-butt');
     var infoDiv = document.getElementById('info-section"');
     var infoTextBox = document.getElementById('info-textbox');
+    // Initially hiding the saved results section
     savedResDiv.style.display = 'none';
 
+    // Adding event listeners for various buttons
     clearSelectionButt.addEventListener('click', clearSelection)
     submitButt.addEventListener('click',submit)
     saveButt.addEventListener('click',save)
     clearSavedButt.addEventListener('click',clear)
 
+    // Function to clear radio button selections
     function clearSelection() {
         var radios = document.querySelectorAll('input[type="radio"]');
         radios.forEach(function (radio) {
@@ -27,17 +32,22 @@ document.addEventListener('DOMContentLoaded',function () {
     }
     function submit(){
         var name = nameInput.value
+        // Validating if a name is provided
         if(name===''){
             displayLog("Name not provided",true)
             return;
         }
+        // Validating if the name contains only English letters and is less than 255 characters
         if (!name.match((/^[a-zA-Z]{1,254}$/))){
             displayLog("Name must only have English letters and be less than 255 characters",true)
             return
         }
+        // Disabling buttons during the API call
         setDisabledButton(true)
+        // Displaying results from saved items
         displayFromSavedItems()
         const api = 'https://api.genderize.io/?name=' + name;
+        // Fetching gender prediction from an external API
         fetch(api)
         .then(response => {
             if (!response.ok) {
@@ -50,15 +60,19 @@ document.addEventListener('DOMContentLoaded',function () {
                 throw new Error("Name not available in api");
             }
             console.log(data)
+            // Displaying the prediction and re-enabling buttons
             displayPrediction(data.gender,data.probability)
             setDisabledButton(false)
         })
         .catch(error => {
-           displayLog(error.message,true)
+             // Handling errors and displaying appropriate messages
+            displayLog(error.message,true)
             genderPrediction.textContent = "Gender"
             probPrediction.textContent = "Probability"
+            setDisabledButton(false)
         });
     }
+    // Function to save predictions to local storage
     function save(){
             if(maleRadioChecker.checked===false && femaleRadioChecker.checked===false){
                 if(genderPrediction.textContent !== "Gender" && probPrediction.textContent !== "Probability"){
@@ -85,6 +99,7 @@ document.addEventListener('DOMContentLoaded',function () {
                 }
             }
     }
+    // Function to clear saved items
     function clear(){
         if(nameInput.value !== null && savedResDiv.style.display === 'block'){
             displayLog("Item Removed Successfully",false)
@@ -92,14 +107,17 @@ document.addEventListener('DOMContentLoaded',function () {
             savedTextbox.textContent="No Data"
         }
     }
+    // Function to disable/enable buttons
     function setDisabledButton(isdisable){
         submitButt.disabled = isdisable;
         saveButt.disabled = isdisable;
     }
+    // Function to display gender prediction results
     function displayPrediction(gender, probability){
         genderPrediction.textContent = gender;
         probPrediction.textContent = probability ;
     }
+    // Function to display results from saved item
     function displayFromSavedItems(){
         if(nameInput.value != null){
             item = localStorage.getItem(nameInput.value)
@@ -112,6 +130,7 @@ document.addEventListener('DOMContentLoaded',function () {
             }
         }
     }
+ // Function to display log messages with optional error styling
     function displayLog(logMessage,isError) {
         infoTextBox.textContent = logMessage
         if (isError) {
